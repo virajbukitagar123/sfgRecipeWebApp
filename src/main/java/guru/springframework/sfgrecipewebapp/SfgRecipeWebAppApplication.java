bb1c1313292aa862e887e1e928764a414eba2430
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-16 22:04:27 +0530

    Initial Commit

---
+++ src/main/java/guru/springframework/sfgrecipewebapp/SfgRecipeWebAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgRecipeWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgrecipewebapp/SfgRecipeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
