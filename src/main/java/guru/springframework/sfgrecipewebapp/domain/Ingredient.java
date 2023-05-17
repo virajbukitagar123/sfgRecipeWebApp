6336436f91eeb59d5658959814706f5d9203f3fe
Viraj Bukitagar <viraj.bukitagar@appdynamics.com>
2023-05-17 18:57:06 +0530

    Added UnitOfMeasure.

--- src/main/java/guru/springframework/sfgrecipewebapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/sfgrecipewebapp/domain/Ingredient.java
@@ -9,10 +9,11 @@ public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
     private String description;
     private BigDecimal amount;
-    // private UnitOfMeasure unitOfMeasure;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
 
     @ManyToOne
     private Recipe recipe;
@@ -47,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 }
--- src/main/java/guru/springframework/sfgrecipewebapp/domain/Notes.java
+++ src/main/java/guru/springframework/sfgrecipewebapp/domain/Notes.java
@@ -7,7 +7,7 @@ public class Notes {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
     @OneToOne
     private Recipe recipe;
 
